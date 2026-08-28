a4f6282c557fb6999c97d894b928452581499063
Dzianis <30404545+MrKarlKori@users.noreply.github.com>
2026-08-28 02:40:36 +0200

    Added initial support for PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { NotesProvider } from './context/NotesContext.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
